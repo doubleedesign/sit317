872c138edac32945d27c98dcd79edad4c9c5c172
Leesa Ward <leesa@doubleedesign.com.au>
2026-09-24 15:41:16 +1000

    Fix: Import animation library at the App level so it will build properly

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ import HeroBanner from '@/components/HeroBanner.vue';
 import TextBlock from '@/components/TextBlock.vue';
 import CallToAction from '@/components/CallToAction.vue';
 import FeatureRow from '@/components/FeatureRow.vue';
+import '@doubleedesign/animate-into-view/dist/animate-into-view.css';
+import '@doubleedesign/animate-into-view/dist/animate-into-view.js';
 </script>
 
 <template>
